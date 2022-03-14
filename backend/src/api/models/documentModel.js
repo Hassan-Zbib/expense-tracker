641e14da5e9f55f264ad802db32da725f953e872
Hassan-Zbib <hassan.zbib01@gmail.com>
2022-03-15 00:14:17 +0200

    added dynamic ref on document model

--- backend/src/api/models/documentModel.js
+++ backend/src/api/models/documentModel.js
@@ -2,6 +2,16 @@ const mongoose = require('mongoose')
 
 const documentSchema = mongoose.Schema(
     {
+        model: {
+            type: String,
+            required: [true, 'Please specify a model'],
+            enum: ['Expense', 'Income']
+        },
+        extendes:  {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            refPath: 'model',
+        },
         notes: {
             type: String,
             required: false,
@@ -11,7 +21,10 @@ const documentSchema = mongoose.Schema(
             type: Date,
             required: [true, 'Please add a date'],
         },
-        document: Object
+        document: {
+            type: Object,
+            // required: true,
+        }
     },
     {
         timestamps: true,
