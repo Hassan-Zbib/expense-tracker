b090bbaaf5c76719b04d0b5aff9ab8c62fbda9e0
Hassan-Zbib <hassan.zbib01@gmail.com>
2022-03-14 14:37:22 +0200

    added express app with mongo conn

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+// const { errorHandler } = require('./api/middlewares/errorMiddleware')
+const  connectDB = require('./config/db')
+const port = process.env.PORT || 5000
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+
+// For Non JSON inputs
+// app.use(express.urlencoded({ extended: false }))
+
+// Routes
+// app.use('/api', require('./api/routes'))
+
+// app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
