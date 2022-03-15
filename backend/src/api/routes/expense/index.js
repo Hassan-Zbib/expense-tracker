cd3eb3a7f3f4fe397908b7b6ee5a1e0d91f74f33
Hassan-Zbib <hassan.zbib01@gmail.com>
2022-03-15 03:01:01 +0200

    setup expenses routes

--- backend/src/api/routes/expense/index.js
+++ backend/src/api/routes/expense/index.js
@@ -1,11 +1,22 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
 
-const {} = require('../../controllers/expenseController')
+const {
+  getExpenses,
+  setExpense,
+  updateExpense,
+  deleteExpense,
+} = require("../../controllers/expenseController")
 
-const { protect } = require('../../middlewares/authMiddleware')
+const { protect } = require("../../middlewares/authMiddleware")
 
-// Routes on /api/expenses/ 
+// Routes on /api/expenses/
+router.route("/")
+        .get(protect, getExpenses)
+        .delete(protect, deleteExpense)
 
+router.route("/:id")
+        .post(protect, setExpense)
+        .put(protect, updateExpense)
 
 module.exports = router
