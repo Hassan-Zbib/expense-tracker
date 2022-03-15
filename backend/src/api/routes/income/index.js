f968a4efe7cc14669d64682fd9f73e9df9680bae
Hassan-Zbib <hassan.zbib01@gmail.com>
2022-03-15 03:12:30 +0200

    setup incomes routes

--- backend/src/api/routes/income/index.js
+++ backend/src/api/routes/income/index.js
@@ -1,11 +1,22 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
 
-const {} = require('../../controllers/incomeController')
+const {
+  getIncomes,
+  setIncome,
+  updateIncome,
+  deleteIncome,
+} = require("../../controllers/incomeController")
 
-const { protect } = require('../../middlewares/authMiddleware')
+const { protect } = require("../../middlewares/authMiddleware")
 
-// Routes on /api/incomes/ 
+// Routes on /api/incomes/
+router.route("/")
+        .get(protect, getIncomes)
+        .delete(protect, deleteIncome)
 
+router.route("/:id")
+        .post(protect, setIncome)
+        .put(protect, updateIncome)
 
 module.exports = router
