c8ac056046e65bfcc314bc3959f254692a88d851
Hassan-Zbib <hassan.zbib01@gmail.com>
2022-03-16 04:49:58 +0200

    added to expenses routes

--- backend/src/api/routes/expense/index.js
+++ backend/src/api/routes/expense/index.js
@@ -6,14 +6,20 @@ const {
   setExpense,
   updateExpense,
   deleteExpense,
+  importExpense,
+  exportExpense
 } = require("../../controllers/expenseController")
 
 const { protect } = require("../../middlewares/authMiddleware")
+const { importCSV } = require("../../middlewares/csvMiddleware")
 
 // Routes on /api/expenses/
 router.route("/")
         .post(protect, setExpense)
         .get(protect, getExpenses)
+
+router.post("/import", protect, importCSV.single('file'), importExpense)
+router.get("/export", protect, exportExpense)
 
 router.route("/:id")
         .delete(protect, deleteExpense)
