b9fbee61488d7126de8720b0b447cbc13b6406d4
Hassan-Zbib <hassan.zbib01@gmail.com>
2022-03-21 03:52:55 +0200

    added footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import { Paper, Typography } from "@mui/material"
+import { makeStyles } from '@mui/styles'
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+        padding: '5px',
+        backgroundColor: theme.palette.background.main,
+      },
+      footer: {
+        marginTop: '5px',
+      }
+}))
+
+
+
+const Footer = () => {
+
+const classes = useStyles()
+
+  return (
+      <div className={classes.footer}>
+      <Paper className={classes.paper} square elevation={0}>
+      <Typography variant="h5" component="h3">
+          React App with Material UI
+        </Typography>
+        <Typography component="p">
+          @2018 All right reserved
+        </Typography>
+        
+      </Paper>
+      </div>
+  )
+}
+
+export default Footer
+
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,13 @@
-import Navbar from '../components/Navbar'
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
 
 const Home = () => {
   return (
-  <Navbar />
-    );
-};
-export default Home;
+    <>
+      <Navbar />
+
+      <Footer />
+    </>
+  )
+}
+export default Home
