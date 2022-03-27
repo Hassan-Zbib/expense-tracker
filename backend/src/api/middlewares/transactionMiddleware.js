51f52a6d98747ee4e73df1aabbc90b2899e4c84e
Hassan Zbib <hassan.zbib01@gmail.com>
2022-03-28 00:33:05 +0300

    fixed expenses in transct middledware

--- backend/src/api/middlewares/transactionMiddleware.js
+++ backend/src/api/middlewares/transactionMiddleware.js
@@ -2,7 +2,7 @@ const User = require('../models/userModel')
 
 async function updateUserIncome(doc) {
 
-    const user = await User.findById(doc.user._id).select('-password')
+    const user = await User.findById(doc.user._id)
 
     if (doc.tempAmount) {
         const diff = doc.amount - doc.tempAmount
@@ -17,7 +17,7 @@ async function updateUserIncome(doc) {
 
 async function truncateUserIncome(doc) {
 
-    const user = await User.findById(doc.user._id).select('-password')
+    const user = await User.findById(doc.user._id)
 
     user.transactions -= 1
     user.totalIncome -= doc.amount
@@ -27,13 +27,13 @@ async function truncateUserIncome(doc) {
 
 async function updateUserExpense(doc) {
 
-    const user = await User.findById(doc.user._id).select('-password')
+    const user = await User.findById(doc.user._id)
 
     if (doc.tempAmount) {
         const diff = doc.amount - doc.tempAmount
-        user.totalIncome += diff
+        user.totalExpenses += diff
     } else {
-        user.totalExpense += doc.amount
+        user.totalExpenses += doc.amount
         user.transactions += 1
     }
 
@@ -42,7 +42,7 @@ async function updateUserExpense(doc) {
 
 async function truncateUserExpense(doc) {
 
-    const user = await User.findById(doc.user._id).select('-password')
+    const user = await User.findById(doc.user._id)
 
     user.transactions -= 1
     user.totalExpense -= doc.amount
