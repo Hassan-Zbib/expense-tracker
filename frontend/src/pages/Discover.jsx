9190e2c59832842dbed6c698f09baeb28c39fe0c
Hassan-Zbib <hassan.zbib01@gmail.com>
2022-03-28 23:55:15 +0300

    finished discover Page

--- frontend/src/pages/Discover.jsx
+++ frontend/src/pages/Discover.jsx
@@ -1,21 +1,31 @@
 import { Grid, Typography } from "@mui/material"
 import DiscoverTable from "../components/DiscoverTable"
 import { useTheme } from "@mui/styles"
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from "react-redux"
 import { useEffect } from "react"
-import {  } from '../features/stats/statsSlice'
-
+import { getDiscoverUsers } from "../features/stats/statsSlice"
 
 const Discover = () => {
+  const theme = useTheme()
   const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(aFunction())
+    dispatch(getDiscoverUsers())
   }, [])
 
-  const { data } = useSelector(state => state.stats)
+  const { data } = useSelector((state) => state.stats)
 
+  if (data[0]) {
+    return (
+      <>
+        <Typography variant="h4" fontWeight="bold">
+          Discover
+        </Typography>
 
-  return <div>{data}</div>;
-};
-export default Discover;
+        <DiscoverTable rows={data}></DiscoverTable>
+      </>
+    )
+  }
+  return null
+}
+export default Discover
