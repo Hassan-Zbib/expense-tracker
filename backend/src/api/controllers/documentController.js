e6bc34db66c8a8d86766abf5983653f5f27dddb2
Hassan-Zbib <hassan.zbib01@gmail.com>
2022-03-31 05:30:32 +0300

    added upload middleware to docs controllers

--- backend/src/api/controllers/documentController.js
+++ backend/src/api/controllers/documentController.js
@@ -1,67 +1,88 @@
-const asyncHandler = require('express-async-handler')
-const Document = require('../models/documentModel')
+const asyncHandler = require("express-async-handler")
+const Document = require("../models/documentModel")
 
 // @desc    Get documents for a transaction
 // @route   GET /api/documents/:model/:id
 // @access  Private
 const getDocuments = asyncHandler(async (req, res) => {
-    const docs = await Document.find({ model: req.params.model, extends: req.params.id })
-  
-    res.status(200).json(docs)
+  if (!req.params.model || !req.params.id) {
+    res.status(400)
+    throw new Error("Please add all fields")
+  }
+  const docs = await Document.find({
+    model: req.params.model,
+    extends: req.params.id,
   })
-  
+
+  res.status(200).json(docs)
+})
+
 // @desc    Set document for a transaction
 // @route   POST /api/documents
 // @access  Private
 const setDocument = asyncHandler(async (req, res) => {
+  const link = req.file.location
 
-const doc = await Document.create({
+  if (!link) {
+    res.status(400)
+    throw new Error("Please Try Again")
+  }
+
+  const doc = await Document.create({
     model: req.body.model,
-    notes: req.body.type,
+    notes: req.body.notes,
     date: req.body.date,
-})
+    document: link,
+  })
 
-res.status(200).json(expense)
+  res.status(200).json(doc)
 })
 
 // @desc    Update document
 // @route   PUT /api/documents/:id
 // @access  Private
 const updateDocument = asyncHandler(async (req, res) => {
-const doc = await Document.findById(req.params.id)
+  const doc = await Document.findById(req.params.id)
+  const link = req.file.location
 
-if (!doc) {
+  if (!doc) {
     res.status(400)
-    throw new Error('Document not found')
-}
+    throw new Error("Document not found")
+  }
 
-doc.notes = req.body.notes
-doc.date = req.body.date
+  if (!link) {
+    res.status(400)
+    throw new Error("Please Try Again")
+  }
 
-const updatedDoc = await doc.save()
+  doc.notes = req.body.notes
+  doc.date = req.body.date
+  doc.document = link
 
-res.status(200).json(updatedDoc)
+  const updatedDoc = await doc.save()
+
+  res.status(200).json(updatedDoc)
 })
 
 // @desc    Delete document
 // @route   DELETE /api/documents/:id
 // @access  Private
 const deleteDocument = asyncHandler(async (req, res) => {
-const doc = await Document.findById(req.params.id)
+  const doc = await Document.findById(req.params.id)
 
-if (!doc) {
+  if (!doc) {
     res.status(400)
-    throw new Error('Document not found')
-}
+    throw new Error("Document not found")
+  }
 
-await doc.remove()
+  await doc.remove()
 
-res.status(200).json({ id: req.params.id })
+  res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
-    getDocuments,
-    setDocument,
-    updateDocument,
-    deleteDocument,
-  }
+  getDocuments,
+  setDocument,
+  updateDocument,
+  deleteDocument,
+}
--- backend/src/api/routes/document/index.js
+++ backend/src/api/routes/document/index.js
@@ -1,22 +1,24 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
 
 const {
-    getDocuments,
-    setDocument,
-    updateDocument,
-    deleteDocument,
-  } = require("../../controllers/documentController")
+  getDocuments,
+  setDocument,
+  updateDocument,
+  deleteDocument,
+} = require("../../controllers/documentController")
 
 const { protect } = require("../../middlewares/authMiddleware")
+const { upload } = require("../../middlewares/multerS3Middleware")
 
 // Routes on /api/documents/
-router.post("/", protect, setDocument)
+router.post("/", protect, upload.single("file"), setDocument)
 
 router.get("/:model/:id", protect, getDocuments)
 
-router.route("/:id")
-    .delete(protect, deleteDocument)
-    .put(protect, updateDocument)
+router
+  .route("/:id")
+  .delete(protect, deleteDocument)
+  .put(protect, upload.single("file"), updateDocument)
 
 module.exports = router
