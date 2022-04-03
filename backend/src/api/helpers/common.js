a7b8a1b1779af19ab2308ee71b3722b02ee646a2
Hassan-Zbib <hassan.zbib01@gmail.com>
2022-04-03 06:50:44 +0300

    fixed import Validation

--- backend/src/api/helpers/common.js
+++ backend/src/api/helpers/common.js
@@ -1,55 +1,68 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken")
 
 const generateToken = (id) => {
-    return jwt.sign(
-        { id },
-        process.env.JWT_SECRET,
-        { expiresIn: '30d' }
-    )
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" })
 }
 
 // custom setters don't work with arrow functions
 function amountCustomSetter(newValue) {
-    if (this.amount) {
-        this.tempAmount = this.amount
-    }
-    return newValue
+  if (this.amount) {
+    this.tempAmount = this.amount
+  }
+  return newValue
+}
+
+const padTo2Digits = (num) => {
+  return num.toString().padStart(2, "0")
 }
 
 const validateImport = (source, id) => {
-    let data = []
-    let totalAmount = 0
+  let data = []
+  let totalAmount = 0
+  const errMsg = {
+    message: "CSV data are not valid",
+  }
 
-    source.forEach(row => {
-        let record = {
-            user: id,
-            type: row.Type,
-            amount: parseInt(row.Amount),
-            date: row.Date
-        }
-
-        if(record.date) {
-            let date = new Date(record.date)
-            let isDate = date instanceof Date
-            if(!isDate){
-                res.status(400)
-                throw new Error(`the record of type '${record.type}' and amount of '${record.amount}' does not have a valid date`)
-            }
-        }
-
-        totalAmount += record.amount
-        data.push(record)
-
-    })
-
-    return {
-        data,
-        totalAmount
+  source.forEach((row) => {
+    if (!row.Type || !row.Amount || !row.Date) {
+        console.log(errMsg)
     }
+
+    if (parseInt(row.Amount) <= 0) {
+        console.log(errMsg)
+    }
+
+    const [date, month, year] = row.Date.split("/")
+
+    const isoStr = `${year}-${padTo2Digits(month)}-${padTo2Digits(
+      date
+    )}T00:00:00.000Z`
+
+    const dateParsed = new Date(Date.parse(isoStr))
+
+    if (!(dateParsed instanceof Date) || dateParsed.toISOString() !== isoStr) {
+        console.log(errMsg)
+    }
+
+    let record = {
+      user: id,
+      type: row.Type,
+      amount: parseInt(row.Amount),
+      date: isoStr,
+    }
+
+    totalAmount += record.amount
+    data.push(record)
+  })
+
+  return {
+    data,
+    totalAmount,
+  }
 }
 
 module.exports = {
-    generateToken,
-    amountCustomSetter,
-    validateImport
+  generateToken,
+  amountCustomSetter,
+  validateImport,
 }
