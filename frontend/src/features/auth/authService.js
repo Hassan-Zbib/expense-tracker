0af63578fa72c1c15a178cb3e0dbc6c1875483fa
Hassan-Zbib <hassan.zbib01@gmail.com>
2022-04-07 08:21:39 +0300

    moved react proxy to axios config

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,4 +1,4 @@
-import axios from "axios"
+import axios from "../../helpers/axios"
 
 const BASE_URL = "/users"
 
--- frontend/src/features/income/incomeService.js
+++ frontend/src/features/income/incomeService.js
@@ -1,4 +1,4 @@
-import axios from "axios"
+import axios from "../../helpers/axios"
 import fileDownload from "js-file-download"
 import { format } from "date-fns"
 
